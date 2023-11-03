403062262dc986a0118e24846573622de11b974f
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2023-11-03 12:21:05 +0530

    .

---
+++ ReactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
