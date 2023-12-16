55ce02d11b1f595e52b2430fab0c73e9068c1b02
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2023-12-16 17:03:59 +0530

    .

---
+++ Practice-Context2/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./Contexts/theme";
+import { useEffect } from "react";
+import ThemeButton from "./Components/ThemeButton";
+import Card from "./Components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <h1 className="bg-gray-400 text-3xl p-4 ">One Piece</h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
