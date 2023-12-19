57002c44a87e658663a09d320ddf30fee0b4bd16
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2023-12-19 11:56:08 +0530

    .

---
+++ Practice-Redux/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from "./Components/AddTodo";
+import Todos from "./Components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
