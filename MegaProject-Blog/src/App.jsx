ec7de2d12b0d0a4213d532876aed7e8700e5fd81
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2023-12-23 18:37:57 +0530

    .

--- MegaProject-Blog/src/App.jsx
+++ MegaProject-Blog/src/App.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/authentication";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A Blog app with appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
--- MegaProject-Blog/src/appwrite/authentication.js
+++ MegaProject-Blog/src/appwrite/authentication.js
@@ -36,7 +36,7 @@ export class AuthService {
         }
     }
 
-    async getAccountUser() {
+    async getCurrentUser() {
         try {
             return await this.account.get();
         } catch (error) {
