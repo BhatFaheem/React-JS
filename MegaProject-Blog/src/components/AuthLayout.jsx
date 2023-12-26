50ae5dd2c3e427f71e081136d56fcd825820917c
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2023-12-26 13:08:18 +0530

    .

--- MegaProject-Blog/src/components/AuthLayout.jsx
+++ MegaProject-Blog/src/components/AuthLayout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSelector } from "react-router-dom";
 
-function AuthLayout() {
-  return (
-    <div>AuthLayout</div>
-  )
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authentication !== authStatus) {
+      navigate("/login");
+    } else if (!authentication && authentication !== authStatus) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+  return <div>AuthLayout</div>;
 }
-
-export default AuthLayout
