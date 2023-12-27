0e7d2faf4ba8d2e28a0dd082e1a7af405d2fd319
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2023-12-27 12:18:39 +0530

    .

--- MegaProject-Blog/src/components/index.js
+++ MegaProject-Blog/src/components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 export default { Header, Footer, Container, Logo, LogoutBtn };
