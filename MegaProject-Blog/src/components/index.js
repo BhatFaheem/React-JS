9de9261156c276bc3178ba2a72932d7a7003a4df
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2023-12-28 11:42:23 +0530

    .

--- MegaProject-Blog/src/components/index.js
+++ MegaProject-Blog/src/components/index.js
@@ -4,4 +4,7 @@ import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-export default { Header, Footer, Container, Logo, LogoutBtn };
+import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
+import PostForm from "../components/post-form/PostForm"
+export default { Header, Footer, Container, Logo, LogoutBtn, RTE, SignUp, Login , PostForm };
