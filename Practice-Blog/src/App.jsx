af93578f7dc7a7e4afbdbe91e160d21887a21c3e
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2024-01-04 18:29:52 +0530

    .

--- Practice-Blog/src/App.jsx
+++ Practice-Blog/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>One Piece </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
