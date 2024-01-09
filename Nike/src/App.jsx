6b21224097db58ffe4a5c2c4ca92e0ffb2e68421
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2024-01-09 11:25:35 +0530

    .

--- Nike/src/App.jsx
+++ Nike/src/App.jsx
@@ -1,14 +1,41 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReview,
+  Subscribe,
+  Footer,
+} from "./sections/index";
+
 const App = () => (
   <main>
     Nav
-    <section className="xl:padding-1 wide: padding-r padding-b">Hero</section>
-    <section className="padding">Popular-Products</section>
-    <section className="padding">Super-Quality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="bg-pale-blue padding">CustomerReview</section>
-    <section className="padding-x sm: py-32 p-16 w-full">Subscribe</section>
-    <section className="padding-x padding-t bg-black pb-8">Footer</section>
+    <section className="xl:padding-1 wide: padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReview />
+    </section>
+    <section className="padding-x sm: py-32 p-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-t bg-black pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
---
+++ Nike/src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReview from './CustomerReview'
+import Hero from './Hero'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import SpecialOffer from './SpecialOffer'
+
+export {
+    CustomerReview,
+    Hero,
+    Footer,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SuperQuality,
+    SpecialOffer,
+};
