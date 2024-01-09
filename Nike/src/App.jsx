c4a0d5a600578416f645168079d9e29a46cc01a2
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2024-01-09 11:10:32 +0530

    .

--- Nike/src/App.jsx
+++ Nike/src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main>
+    Nav
+    <section className="xl:padding-1 wide: padding-r padding-b">Hero</section>
+    <section className="padding">Popular-Products</section>
+    <section className="padding">Super-Quality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">SpecialOffer</section>
+    <section className="bg-pale-blue padding">CustomerReview</section>
+    <section className="padding-x sm: py-32 p-16 w-full">Subscribe</section>
+    <section className="padding-x padding-t bg-black pb-8">Footer</section>
+  </main>
+);
+
+export default App;
